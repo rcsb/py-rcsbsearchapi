0861900030e192c3682da7900695f498256c8b72
Dennis Piehl <piehld@gmail.com>
2023-06-08 12:09:31 -0400

    Update setup and azure files

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 packages = []
-thisPackage = "rcsbsearchapi.search"
+thisPackage = "rcsbsearchapi"
 
 with open("rcsbsearchapi/__init__.py", "r", encoding="utf-8") as fd:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
@@ -28,39 +28,49 @@ if not version:
 setup(
     name=thisPackage,
     version=version,
-    description="RCSB PDB Python Package for supporting our search api",
+    description="Python package interface for the RCSB PDB search API service",
     long_description_content_type="text/markdown",
     long_description=longDescription,
-    author="Spencer Bliven",
-    author_email="spencer.bliven@gmail.com",
+    author="Dennis Piehl",
+    author_email="dennis.piehl@rcsb.org",
     url="https://github.com/rcsb/py-rcsb_api_search",
     #
     license="BSD 3-Clause",
     classifiers=[
-        "Development Status :: 4 - Beta",
-        # 'Development Status :: 5 - Production/Stable',
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "License :: OSI Approved :: Apache Software License", #don't know if this is correct
         "Programming Language :: Python",
         # "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
+        "Development Status :: 4 - Beta",
+        # 'Development Status :: 5 - Production/Stable',
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Natural Language :: English",
+        "License :: OSI Approved :: BSD License",
+        "Typing :: Typed",
     ],
     entry_points={"console_scripts": []},
     #
     install_requires=packagesRequired,
-    packages=find_packages(exclude=["rcsb.mock-data", "tests", "tests-*", "tests.*"]), #mock data?? - Santi
+    packages=find_packages(exclude=["tests", "tests-*", "tests.*"]),
     package_data={
         # If any package contains *.md or *.rst ...  files, include them:
-        "": ["*.md", "*.rst", "*.txt", "*.cfg"]
+        "": ["*.md", "*.rst", "*.txt", "*.cfg", "resources/*"]
     },
     #
     test_suite="tests",
     tests_require=["tox"],
     #
     # Not configured ...
-    extras_require={"dev": ["check-manifest"], "test": ["coverage"]},
+    extras_require={
+        "dev": ["check-manifest"],
+        "test": ["coverage"],
+        "tests": ["tox", "pylint", "black>=21.5b1", "flake8"],
+        "progressbar": ["tqdm"],
+        # should match docs/requirements.txt
+        "docs": ["sphinx", "sphinx-rtd-theme", "myst-parser"],
+    },
     # Added for
     command_options={"build_sphinx": {"project": ("setup.py", thisPackage), "version": ("setup.py", version), "release": ("setup.py", version)}},
     # This setting for namespace package support -
