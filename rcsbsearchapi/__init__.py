f9de54d83798a8c192edcc2662ed7afb22915ad2
Ivana Truong <truon322@umn.edu>
2025-03-25 16:36:21 -0400

    simplify import

--- rcsbsearchapi/__init__.py
+++ rcsbsearchapi/__init__.py
@@ -3,13 +3,13 @@
 from typing import List
 from .search import Terminal, SCHEMA  # noqa: F401
 from .search import Attr, AttributeQuery, Group, Query, TextQuery
-import warnings
+from warnings import warn
 
 __version__ = "2.0.1"
 
 rcsb_attributes = SCHEMA.rcsb_attributes
 
-warnings.warn(
+warn(
     """Please migrate to the use of our new and improved package, rcsb-api (https://rcsbapi.readthedocs.io/en/latest/),
     which contains all the same functionalities as rcsbsearchapi and more! New features will only be added to the new rcsb-api package.
     For more details, see https://github.com/rcsb/py-rcsbsearchapi/issues/51.""",
